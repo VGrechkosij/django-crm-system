84855c76e2c4e443ab38edf8c6f50fb018dff92b
Vladislav <grechkosij.vlados@gmail.com>
2026-05-15 16:46:53 +0300

    Register custom user in admin

--- crm/admin.py
+++ crm/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        "username",
+        "phone",
+        "email",
+        "first_name",
+        "last_name",
+        "position",
+        "role",
+        "is_staff",
+        "is_active",
+    )
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Additional info",
+            {
+                "fields": (
+                    "phone",
+                    "position",
+                    "role"
+                )
+            }
+        ),
+    )
+
+    search_fields = (
+        "username",
+        "phone",
+        "email",
+        "first_name",
+        "last_name",
+        "position",
+        "role",
+    )
+
+    list_filter = (
+        "is_staff",
+        "is_superuser",
+        "is_active",
+        "position",
+        "role",
+        "groups",
+    )
+
+    ordering = ("username",)
